1070f2161812c5073ee8f684497c9d0a07854e38
Connor Tumbleson <connor@sourcetoad.com>
2023-01-04 08:58:09 -0500

    build: initial tamagui setup

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from '@tamagui/text'
+
+export {
+    Text
+} from '@tamagui/core'
---
+++ src/setup.ts
@@ -0,0 +1,7 @@
+import { setupReactNative } from '@tamagui/core'
+import { Text, View } from 'react-native'
+
+setupReactNative({
+    View,
+    Text,
+})
