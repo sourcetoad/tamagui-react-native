efdf02fc79482b10540182cbb7123b55643955fa
Connor Tumbleson <connor@sourcetoad.com>
2023-01-04 11:54:26 -0500

    feat: add configbase/lucide icons

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export * from './setup'
 
 export * from '@tamagui/button'
 export * from '@tamagui/core'
+export * from '@tamagui/config-base'
+export * from '@tamagui/lucide-icons'
 export * from '@tamagui/stacks'
 export * from '@tamagui/text'
 
