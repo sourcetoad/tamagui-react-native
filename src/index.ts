93a71c054b06e055ee84be776d1f2b8bc9a729eb
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2023-01-04 11:58:02 -0500

    Add GetButtonSized, ConfigBase, LucideIcons (#3)
    * feat: add configbase/lucide icons
    
    * build: add get-button-sized

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 export * from './setup'
 
 export * from '@tamagui/button'
+export * from '@tamagui/get-button-sized'
 export * from '@tamagui/core'
+export * from '@tamagui/config-base'
+export * from '@tamagui/lucide-icons'
 export * from '@tamagui/stacks'
 export * from '@tamagui/text'
 
