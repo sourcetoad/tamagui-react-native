3d58e4d66e1c071905b5dd2d334920e4f238c3a1
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2023-01-04 13:03:21 -0500

    fix: alias export LucideIcons (#5)

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,10 @@ export * from '@tamagui/button'
 export * from '@tamagui/get-button-sized'
 export * from '@tamagui/core'
 export * from '@tamagui/config-base'
-export * from '@tamagui/lucide-icons'
 export * from '@tamagui/stacks'
 export * from '@tamagui/text'
+
+export * as LucideIcons from '@tamagui/lucide-icons'
 
 export {
     Text,
