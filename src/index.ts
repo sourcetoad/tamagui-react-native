65b086dd82f602ea1736b952c43604e16d2cba48
ian-wd <54297307+ian-wd@users.noreply.github.com>
2025-09-10 18:25:08 +0800

    feat: bump tamagui to 1.132.x (#78)
    * chore: bump tamagui to v1.132.23
    
    * feat: add "tamagui/theme` package
    
    * feat: export theme-builder instead of theme instead

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ export * from '@tamagui/scroll-view'
 export * from '@tamagui/radio-group'
 export * from '@tamagui/label'
 
+export * as ThemeBuilder from '@tamagui/theme-builder'
 export * as LucideIcons from '@tamagui/lucide-icons'
 
 export {
